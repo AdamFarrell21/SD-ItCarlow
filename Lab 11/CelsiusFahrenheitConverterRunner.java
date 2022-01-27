7843d1bc8658880732fbdd88f0e5fced26333812
AdamFarrell21 <91830026+AdamFarrell21@users.noreply.github.com>
2022-01-27 09:35:57 +0000

    Driver

---
+++ Lab 11/CelsiusFahrenheitConverterRunner.java
@@ -0,0 +1,13 @@
+package Lab11;
+
+import javax.swing.*;
+
+public class CelsiusFahrenheitConverterRunner {
+    public static void main(String[] args) {
+    CelsiusFahrenheitConverter celsiusFahrenheitConverter = new CelsiusFahrenheitConverter();
+    celsiusFahrenheitConverter.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    celsiusFahrenheitConverter.setSize(400, 250);
+    celsiusFahrenheitConverter.setVisible(true);
+    celsiusFahrenheitConverter.setLocationRelativeTo(null);
+    }
+}
